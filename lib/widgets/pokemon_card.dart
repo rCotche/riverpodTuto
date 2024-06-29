043dcc1b396209bc8661f38f6804218586a3fb04
rCotche <roshan.cotche@gmail.com>
2024-06-29 16:09:05 +0400

    card ui in dev

--- lib/widgets/pokemon_card.dart
+++ lib/widgets/pokemon_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_example/models/pokemon.dart';
+import 'package:riverpod_example/providers/pokemon_data_providers.dart';
 
 class PokemonCard extends ConsumerWidget {
   final String pokemonUrl;
@@ -9,6 +11,40 @@ class PokemonCard extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    final pokemon = ref.watch(pokemonDataProvider(pokemonUrl));
+
+    return pokemon.when(data: (data) {
+      return _card(
+        context,
+        false,
+        data,
+      );
+    }, error: (error, stackTrace) {
+      return Text(
+        "Error: $error",
+      );
+    }, loading: () {
+      return _card(
+        context,
+        true,
+        null,
+      );
+    });
+  }
+
+  Widget _card(BuildContext context, bool isLoading, Pokemon? pokemon) {
+    return Container(
+      margin: EdgeInsets.symmetric(
+        horizontal: MediaQuery.sizeOf(context).width * 0.03,
+        vertical: MediaQuery.sizeOf(context).height * 0.01,
+      ),
+      padding: EdgeInsets.symmetric(
+        horizontal: MediaQuery.sizeOf(context).width * 0.03,
+        vertical: MediaQuery.sizeOf(context).height * 0.01,
+      ),
+      decoration: BoxDecoration(
+        color: Theme.of(context).primaryColor,
+      ),
+    );
   }
 }
